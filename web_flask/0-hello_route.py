aa76a43879d22ff03a83eb1ae25f7af679464e21
Ashley <ajchapman777@gmail.com>
2024-03-12 19:58:27 +0000

    0-hello_route.py

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+
 """ Starts a Flash Web Application """
 
 from flask import Flask
@@ -15,31 +16,9 @@ def hello_hbnb():
 
     return 'Hello HBNB!'
 
+
 if __name__ == "__main__":
 
     """ Main Function """
 
-    app.run(host='0.0.0.0', port=5000)#!/usr/bin/python3
-
-    """ Starts a Flash Web Application """
-
-    from flask import Flask
-
-    app = Flask(__name__)
-
-
-
-    @app.route('/', strict_slashes=False)
-
-    def hello_hbnb():
-
-            """ Prints a Message when / is called """
-
-                return 'Hello HBNB!'
-
-
-            if __name__ == "__main__":
-
-                    """ Main Function """
-
-                        app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5000)
