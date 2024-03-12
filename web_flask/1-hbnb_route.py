3d65a66d04bfb26dc0a58c1f99bbe9db0a3d0f60
Ashley <ajchapman777@gmail.com>
2024-03-12 21:30:00 +0000

    removing spaces for 1-hbnb

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -7,22 +7,18 @@ from flask import Flask
 app = Flask(__name__)
 
 
-
 @app.route('/', strict_slashes=False)
 
 def hello_hbnb():
-
-    """ Prints a Message when / is called """
+""" Prints a Message when / is called """
 
     return 'Hello HBNB!'
-
 
 
 @app.route('/hbnb', strict_slashes=False)
 
 def hbnb():
-
-    """ Prints a Message when /hbnb is called """
+""" Prints a Message when /hbnb is called """
 
     return 'HBNB'
 
