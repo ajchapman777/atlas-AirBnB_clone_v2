8590133a5967364150fc53884ab8fc3d268ec206
Ashley <ajchapman777@gmail.com>
2024-03-12 21:32:26 +0000

    1-hbnb_route.py

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -1,17 +1,17 @@
 #!/usr/bin/python3
-
 """ Starts a Flash Web Application HBNB"""
 
 from flask import Flask
+
 
 app = Flask(__name__)
 
 
 @app.route('/', strict_slashes=False)
 
+
 def hello_hbnb():
 """ Prints a Message when / is called """
-
     return 'Hello HBNB!'
 
 
@@ -20,11 +20,10 @@ def hello_hbnb():
 def hbnb():
 """ Prints a Message when /hbnb is called """
 
-    return 'HBNB'
+return 'HBNB'
 
 
 if __name__ == "__main__":
-
-    """ Main Function """
+""" Main Function """
 
     app.run(host='0.0.0.0', port=5000)
