c3cf35be04f99243eb1fe4a9179db247459aa7f2
Ashley <ajchapman777@gmail.com>
2024-03-12 15:43:37 -0500

    0-hello_route.py

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """ Starts a Flash Web Application """
 from flask import Flask
-app = Flask(__name_)
+app = Flask(__name__)
 
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
